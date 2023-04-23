258e2efc51ef67af26fa9e9522a7bcd687fdbf97
Jeon <jeoninseong8140@gmail.com>
2023-04-23 16:40:26 +0900

    feat: about page
    feat about page

---
+++ sol/src/app/about/page.tsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <section className="container prose p-10 mx-auto">
+      <h1>About</h1>
+
+
+      <h3>이력</h3>
+        <ul>
+          <li>인하대학교 컴퓨터공학과 재학중(2017.3 ~ 2024.3 졸업 예정)</li>
+          <li>네이버 커넥트재단 (부스트캠프 웹·모바일 7기) 멤버십 | 웹 풀스택</li>
+          <li>백석고등학교 졸업(2014.3 ~ 2017.2)</li>
+        </ul>
+    </section>
+  );
+}
+
+export default About;
